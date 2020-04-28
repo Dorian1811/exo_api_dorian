afa183a692f88dcc062e728da3ce2b7c5541e43d
Dorian <doriangalant@gmail.com>
2020-04-28 14:10:50 +0200

    modifs final

--- js/script.js
+++ js/script.js
@@ -29,67 +29,72 @@ $('form').submit(function(e)
         data : $(this).serialize(),
         success: function(data){
 
-            let cityTable = $(`
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nom</th>
-                        <th>Codes postaux</th>
-                        <th>Population</th>
-                        <th>Département</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
-            `);
+            if(data.length == 0){
 
-            //console.log(data);
-            data.forEach(function(city){
-            
-            let newCity = $('<tr></tr>');
-            
-            let cityNom = $('<td></td>');
-            cityNom.text(city.nom);
-            
-            let cityCodePostaux = $('<td></td>');
-            cityCodePostaux.html(city.codesPostaux.join('<br>'));
-            
-            let cityPopulation = $('<td></td>');
-            cityPopulation.text(city.population);
-            
-            let cityCodeDepartement = $('<td></td>');
-            cityCodeDepartement.text(city.codeDepartement);
-            
-            newCity.append(cityNom);
-            newCity.append(cityCodePostaux);
-            newCity.append(cityPopulation);
-            newCity.append(cityCodeDepartement);
-                    
-            cityTable.find('tbody').append(newCity);
-            
-            });
+                $('.tab').html('<p class="error" style="color:red">Aucun résultat</p>');
+        
+            }else {
 
-                $('.tab').html(cityTable);
+                let cityTable = $(`
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nom</th>
+                                <th>Codes postaux</th>
+                                <th>Population</th>
+                                <th>Département</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                `);
+            
+                //console.log(data);
+                data.forEach(function(city){
                 
-                $('form').append('<p class="results" style="color:green">Résultats : ' + data.length +'</p>');
+                let newCity = $('<tr></tr>');
+                
+                let cityNom = $('<td></td>');
+                cityNom.text(city.nom);
+                
+                let cityCodePostaux = $('<td></td>');
+                cityCodePostaux.html(city.codesPostaux.join('<br>'));
+                
+                let cityPopulation = $('<td></td>');
+                cityPopulation.text(city.population);
+                
+                let cityCodeDepartement = $('<td></td>');
+                cityCodeDepartement.text(city.codeDepartement);
+                
+                newCity.append(cityNom);
+                newCity.append(cityCodePostaux);
+                newCity.append(cityPopulation);
+                newCity.append(cityCodeDepartement);
+                        
+                cityTable.find('tbody').append(newCity);
+                
+                });
 
-             
-            },
-            error: function(){
-                $('form').prepend('<p class="error" style="color:red">Problème de connexion</p>');
-            },
-            beforeSend: function(){
-
-                setOverlay();
-
-            },
-            complete: function(){
-
-                removeOverlay();
+                    $('.tab').html(cityTable);
+                    
+                    $('form').append('<p class="results" style="color:green">Résultats : ' + data.length +'</p>');
 
             }
+                },
+                error: function(){
+                    $('form').prepend('<p class="error" style="color:red">Problème de connexion</p>');
+                },
+                beforeSend: function(){
 
+                    setOverlay();
+
+                },
+                complete: function(){
+
+                    removeOverlay();
+
+                }
         });  
         
     }
